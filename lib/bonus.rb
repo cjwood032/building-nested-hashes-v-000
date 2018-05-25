d3ab9d567d8c7dd74d97dbb6d52711774eef4ccc
CHRISTOPHER WOOD <cjwood032@gmail.com>
2018-05-25 13:41:53 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ def bonus
   }
 
  def killem
-   epic_tragedy[:montague][:hero][status] = "dead"
-   epic_tragedy[:capulet][:heroine][status] = "dead"
+   epic_tragedy[:montague][:hero][:status] = "dead"
+   epic_tragedy[:capulet][:heroine][:status] = "dead"
    epic_tragedy
 end
 end
